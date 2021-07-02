7621971d4fb7e48074bfc4432bd43ef189827300
HemanthThota39 <hemant.thota@gmail.com>
2021-07-02 16:35:16 +0530

    sum of two numbers

--- first.cpp
+++ first.cpp
@@ -2,6 +2,10 @@
 
 int main()
 {
-    std::cout<<"Hemanth"<<std::endl;
+    int v1,v2;
+    std::cout<<"Hello, Hemanth"<<std::endl;
+    std::cout<<"Enter two numbers"<<std::endl;
+    std::cin>>v1>>v2;
+    std::cout<<"The sum of "<<v1<<" and "<<v2<<" is "<<v1+v2<<std::endl;
     return 0;
 }
