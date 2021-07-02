c477c6b58ed68123c442a1f7eb26905402b7f1e6
HemanthThota39 <hemant.thota@gmail.com>
2021-07-02 20:50:32 +0530

    Typedef and using

--- second.cpp
+++ second.cpp
@@ -13,7 +13,12 @@ int main()
     int &refValue1 = value;//It just acts a synonym
     cout<<refValue1<<endl;
     const int value2 = 100;
-    int *ptr = &value2;//error: this is a plain pointer
     const int *ptr2 = &value2;//This works
+    typedef double wages;
+    wages wage1 = 200.43;
+    using integer = int;
+    integer k = 10;
+    cout<<"integer is "<<k<<endl;
+    cout<<"wage1 is "<<wage1<<endl;
     return 0;    
 }
