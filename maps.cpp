98946a5f90e662f7b4803d178e94bb0bfe7b0cef
HemanthThota39 <hemant.thota@gmail.com>
2021-07-10 19:43:52 +0530

    This is end of map

--- maps.cpp
+++ maps.cpp
@@ -10,8 +10,16 @@ int main()
     mp["chocolate"] = 10;
     mp["biscuit"] = 20;
     mp.insert(pair<string, int> ("cake",30));
+    mp.insert(pair<string, int>("puff", 25));
+    auto it1 = mp.find("chocolate");
+    mp.erase(it1);
     for (auto it=mp.begin(); it!=mp.end(); ++it)
     {
-        cout<<it->first<<"-->>"<<it->second<<endl;    
+        cout<<it->first<<"-->>"<<it->second<<endl;
+
     }
+    cout<<mp.empty()<<endl;
+    
+    cout<<mp.size()<<endl;
+    
 }
