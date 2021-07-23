0db9712b4134b3d52876104d384f0b5191f9652f
HemanthThota39 <76810955+HemanthThota39@users.noreply.github.com>
2021-07-23 23:51:33 +0530

    Added bubblesort algorithm

--- sorting/bubblesort.cpp
+++ sorting/bubblesort.cpp
@@ -8,28 +8,27 @@ void swap(int *a, int *b)
     *b = *a;
     *a = temp;
 }
+//bubble sort algorithm
 void bubblesort(int array[], int len)
 {
-    int flag = 0;
-    do{
+    
+    while (1)
+    {   
+        int no_of_swaps = 0;
         for (int i=0; i<len-1; i++)
-        {   int no_of_swaps = 0;
+        {  
             if (array[i]>array[i+1]){
                 swap(array[i],array[i+1]);
                 no_of_swaps++;
             }
-            if(no_of_swaps == 0)
-                flag = 1;   
         }
-        
-        
-    }while(flag!=1);
-    
+        if(no_of_swaps == 0){break;}     
+    }      
 }
 
 int main()
 {
-    int array[] = {12,34,76,11,2,6,87};
+    int array[] = {64, 34, 25, 12, 22, 11, 90};
     bubblesort(array, sizeof(array)/sizeof(array[0]));
     for (int i=0; i<sizeof(array)/sizeof(array[0]); i++)
     {
